de01b006c27efc5b816b8fafe61ce1d0d99902e1
manueljuan123 <79883680+manueljuan123@users.noreply.github.com>
2024-04-28 16:50:20 -0500

    - Se agregó formulario registro de usuario. - Se cambiaron clases css y se creó nuevo archivo para que sirva de estilo en los formularios de autenticación. - Se agregó la ruta de registro en rutas. - Se agregaron íconos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { LayoutComponent } from './layout/menu-layout/menu-layout.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { HistorialMedicoComponent } from './dashboard/historial-medico/historial-medico.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: LoginComponent
+    },
+    {
+        path: 'registro',
+        component: RegisterComponent
     },
     {
         path: '',
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: '../global-auth-form-style.css'
 })
 export class RegisterComponent {
 
